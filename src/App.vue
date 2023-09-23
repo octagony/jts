661a131f1f61d050aaebc176c101150fbc29de33
octagony <moctagony@gmail.com>
2023-09-23 21:07:20 +0300

    [FEAT] Added Codemirror

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,22 @@
 <script setup lang="ts">
 import { json2ts } from "json-ts";
 import { ref } from "vue";
-const input = ref("");
-const output = ref("");
+import CodeMirror from "codemirror-editor-vue3";
+const inputJSON = ref("");
+const outputTS = ref("");
 const generateTS = () => {
-  const generate = json2ts(input.value);
-  output.value = generate;
+  const generate = json2ts(inputJSON.value);
+  outputTS.value = generate;
 };
 </script>
 <template>
   <v-app>
     <v-container fluid>
-      <v-row align="end" justify="end">
-        <v-textarea
-          clearable
-          label="Label"
-          prepend-icon="$vuetify"
-          variant="outlined"
-          v-model="input"
-          no-resize
-        />
-        <v-textarea
-          clearable
-          label="Label"
-          prepend-icon="$vuetify"
-          variant="outlined"
-          no-resize
-          v-model="output"
-        />
-      </v-row>
-      <v-sheet>
-        <v-btn prepend-icon="$vuetify" variant="tonal" @click="generateTS">
-          Button
-        </v-btn>
-      </v-sheet>
+      <CodeMirror v-model:value="inputJSON" border :height="200" />
+      <CodeMirror v-model:value="outputTS" border :height="200" />
+      <v-btn prepend-icon="$vuetify" variant="tonal" @click="generateTS">
+        Button
+      </v-btn>
     </v-container>
   </v-app>
 </template>
