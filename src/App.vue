0e8b7cea01dfc3f11dbd01d6c4d3066b0343ee49
octagony <moctagony@gmail.com>
2023-09-23 20:22:00 +0300

    [FEAT] Setup basic logic

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
-<script lang="ts">
+<script setup lang="ts">
 import { json2ts } from "json-ts";
-
-const json = `
-  {
-  "name":"John"
-  }
-  `;
-console.log(json2ts(json));
+import { ref } from "vue";
+const input = ref("");
+const output = ref("");
+const generateTS = () => {
+  const generate = json2ts(input.value);
+  output.value = generate;
+};
 </script>
-<template></template>
+<template>
+  <v-app>
+    <v-container fluid>
+      <v-row align="end" justify="end">
+        <v-textarea
+          clearable
+          label="Label"
+          prepend-icon="$vuetify"
+          variant="outlined"
+          v-model="input"
+          no-resize
+        />
+        <v-textarea
+          clearable
+          label="Label"
+          prepend-icon="$vuetify"
+          variant="outlined"
+          no-resize
+          v-model="output"
+        />
+      </v-row>
+      <v-sheet>
+        <v-btn prepend-icon="$vuetify" variant="tonal" @click="generateTS">
+          Button
+        </v-btn>
+      </v-sheet>
+    </v-container>
+  </v-app>
+</template>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,7 +5,6 @@ import "vuetify/styles";
 // Composables
 import { createVuetify } from "vuetify";
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     themes: {
