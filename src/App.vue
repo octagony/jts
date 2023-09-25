3d6c70e3805c56564c79aaf854029be83871f4f3
octagony <moctagony@gmail.com>
2023-09-25 21:58:48 +0300

    [FEAT] Add json-editor-vue package

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
 import { json2ts } from "json-ts";
 import { ref } from "vue";
-import CodeMirror from "codemirror-editor-vue3";
+import JsonEditorVue from "json-editor-vue";
+
 const inputJSON = ref("");
 const outputTS = ref("");
+
 const generateTS = () => {
   const generate = json2ts(inputJSON.value);
   outputTS.value = generate;
+  console.log(outputTS.value);
 };
 </script>
+
 <template>
   <v-app>
     <v-container fluid>
-      <CodeMirror v-model:value="inputJSON" border :height="200" />
-      <CodeMirror v-model:value="outputTS" border :height="200" />
+      <JsonEditorVue
+        v-model="inputJSON"
+        mode="text"
+        :navigationBar="false"
+        :statusBar="false"
+        :main-menu-bar="false"
+      />
       <v-btn prepend-icon="$vuetify" variant="tonal" @click="generateTS">
-        Button
+        Generate Types
       </v-btn>
     </v-container>
   </v-app>
