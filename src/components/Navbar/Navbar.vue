866140f590ba97efac5d738771403e31af7a28ff
octagony <moctagony@gmail.com>
2023-10-07 19:55:15 +0300

    [FEAT] Implemented icon change when switching the theme

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useTheme } from "vuetify/lib/framework.mjs";
 const theme = useTheme();
 
@@ -7,13 +8,19 @@ const toggleTheme = () => {
     ? "lightTheme"
     : "darkTheme";
 };
+
+const currentTheme = computed(() => {
+  return theme.global.name.value === "lightTheme"
+    ? "mdi-weather-sunny"
+    : "mdi-weather-night";
+});
 </script>
 <template>
   <v-app-bar :elevation="2" class="px-4">
     <h1 class="text-primary">JTS</h1>
     <template v-slot:append>
       <v-btn icon="mdi-github"></v-btn>
-      <v-btn icon="mdi-weather-sunny" @click="toggleTheme"></v-btn>
+      <v-btn :icon="currentTheme" @click="toggleTheme"></v-btn>
     </template>
   </v-app-bar>
 </template>
